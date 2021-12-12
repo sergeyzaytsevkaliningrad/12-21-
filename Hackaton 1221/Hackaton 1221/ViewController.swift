4657f04309bc2d645799f91c11bc497b2381969d
sergeyzaytsevkaliningrad <37252069+sergeyzaytsevkaliningrad@users.noreply.github.com>
2021-12-12 13:18:00 +0300

    1
    1

---
+++ Hackaton 1221/Hackaton 1221/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hackaton 1221
+//
+//  Created by Сергей Зайцев on 11.12.2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
